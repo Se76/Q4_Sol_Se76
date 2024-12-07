0475e0df101a082189f3b73ac4469217076a0df9
NickNut <68846529+Se76@users.noreply.github.com>
2024-12-07 20:39:22 +0100

    add functionality to the market

--- market/programs/market/src/lib.rs
+++ market/programs/market/src/lib.rs
@@ -1,16 +1,35 @@
-use anchor_lang::prelude::*;
+mod instructions;
+mod state;
+mod errors;
 
-declare_id!("76ofariHewGmFaaeP1NtuxoEMyyNj1wYxapieW22Cnff");
+use anchor_lang::prelude::*;
+use crate::instructions::*;
+
+
+
+declare_id!("5veWFCgrb8zE3vPHogD3532wvZxvqUqqa2iBw31PjF3T");
 
 #[program]
 pub mod market {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;    
+        Ok(())
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_nft()?;
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_rewards()?;
+        ctx.accounts.close_vault()?;
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
