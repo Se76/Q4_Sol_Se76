f1aeaa8e23527883fad772cf009638260b120a14
NickNut <68846529+Se76@users.noreply.github.com>
2024-12-30 23:19:06 +0100

    add basic tests

--- escrow/tests/escrowtutorial.ts
+++ escrow/tests/escrowtutorial.ts
@@ -8,9 +8,25 @@ describe("escrowtutorial", () => {
 
   const program = anchor.workspace.Escrowtutorial as Program<Escrowtutorial>;
 
+  
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+  it("The vault was created!", async () => {
+    // Add your test here.
+    const tx = await program.methods.make().rpc();
+    console.log("Your transaction signature", tx);
+  });
+  it("The vault was used and closed!", async () => {
+    // Add your test here.
+    const tx = await program.methods.take().rpc();
+    console.log("Your transaction signature", tx);
+  });
+  it("The deal was refunded!", async () => {
+    // Add your test here.
+    const tx = await program.methods.refund().rpc();
     console.log("Your transaction signature", tx);
   });
 });
